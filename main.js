15b787e7f3c3c6e149a92978212b2627ea323823
CharmingNiska <71717872+CharmingNiska@users.noreply.github.com>
2020-10-09 19:23:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+lrightWristY = 0;
 
 function preload()
 {
-  
+  song = loadSound("music.mp3");
 }
 
 function setup() {
@@ -11,6 +15,28 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+     poseNet = ml5.poseNet(video, modelLoaded);
+     poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded (){
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses()
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +"leftWristY = "+ leftWristY);
+        
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +"rightWristY = "+ rightWristY);
+    }
 }
 
 function draw()
@@ -20,5 +46,7 @@ function draw()
 
 function play()
 {
-    
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
